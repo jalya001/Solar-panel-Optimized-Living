f71af0875fb9e444e8adf0851a0c2b577da0962d
anisai <anisai@uio.no>
2025-03-17 22:20:11 +0100

    Fetched data with Ktor Client, and added some neccessary dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -63,4 +63,6 @@ dependencies {
     implementation(libs.ktor.client.okhttp)
     implementation(libs.ktor.client.content.negotiation)
     implementation(libs.ktor.ktor.serialization.kotlinx.json)
+    implementation(libs.ktor.ktor.client.content.negotiation)
+    implementation(libs.kotlinx.serialization.json)
 }
--- app/src/main/java/no/solcellepaneller/data/homedata/ElectricityPriceApi.kt
+++ app/src/main/java/no/solcellepaneller/data/homedata/ElectricityPriceApi.kt
@@ -1,4 +1,28 @@
 package no.solcellepaneller.data.homedata
 
+import io.ktor.client.HttpClient
+import io.ktor.client.call.body
+import io.ktor.client.plugins.contentnegotiation.ContentNegotiation
+import io.ktor.client.request.get
+import io.ktor.serialization.kotlinx.json.json
+import kotlinx.serialization.json.Json
+import no.solcellepaneller.model.electricity.ElectricityPrice
+import java.time.LocalDate
+
 class ElectricityPriceApi {
+    private val client = HttpClient {
+        install(ContentNegotiation) {
+            json(Json { ignoreUnknownKeys = true })
+        }
+    }
+
+    suspend fun fetchPrices(date: LocalDate, priceArea: String): List<ElectricityPrice> {
+        val url = "https://www.hvakosterstrommen.no/api/v1/prices/${date.year}/${date.monthValue}-${date.dayOfMonth}_$priceArea.json"
+        return try {
+            client.get(url).body()
+        } catch (e: Exception) {
+            println("Error fetching electricity prices: ${e.message}")
+            emptyList()
+        }
+    }
 }
