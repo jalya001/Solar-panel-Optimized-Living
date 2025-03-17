86c07c5ac2bad5465283eed2abc2b6afe1b63af9
anisai <anisai@uio.no>
2025-03-17 22:27:05 +0100

    Repository to handle the fetched data

--- app/src/main/java/no/solcellepaneller/data/homedata/ElectricityPriceRepository.kt
+++ app/src/main/java/no/solcellepaneller/data/homedata/ElectricityPriceRepository.kt
@@ -1,2 +1,17 @@
 package no.solcellepaneller.data.homedata
 
+import no.solcellepaneller.model.electricity.ElectricityPrice
+import java.time.LocalDate
+
+class ElectricityPriceRepository(private val priceArea: String) {
+    private val api = ElectricityPriceApi()
+    private var prices: List<ElectricityPrice> = emptyList()
+
+    suspend fun updatePrices(date: LocalDate) {
+        prices = api.fetchPrices(date, priceArea)
+    }
+
+    fun getPrices(): List<ElectricityPrice> {
+        return prices
+    }
+}
