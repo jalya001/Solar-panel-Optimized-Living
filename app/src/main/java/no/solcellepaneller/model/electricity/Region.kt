0a1633271f36f179ffc8e93dc4c09f4aae2f20ee
anisai <anisai@uio.no>
2025-03-24 18:37:50 +0100

    enum-class for regionene, skal brukes i dropdown-meny

---
+++ app/src/main/java/no/solcellepaneller/model/electricity/Region.kt
@@ -0,0 +1,9 @@
+package no.solcellepaneller.model.electricity
+
+enum class Region(val displayName: String, val regionCode: String) {
+    OSLO("Oslo/Øst-Norge", "NO1"),
+    KRISTIANSAND("Kristiansand/Sør-Norge", "NO2"),
+    TRONDHEIM("Trondheim/Midt-Norge", "NO3"),
+    TROMSO("Tromsø/Nord-Norge", "NO4"),
+    BERGEN("Bergen/Vest-Norge", "NO5")
+}
