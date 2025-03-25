109b7c2d1b617ae16837b123333bd486bb610dc8
anisai <anisai@uio.no>
2025-03-25 16:47:32 +0100

    tester om api-kall fungerer som det skal og om strømpriser for valgt region vises

--- app/src/main/java/no/solcellepaneller/MainActivity.kt
+++ app/src/main/java/no/solcellepaneller/MainActivity.kt
@@ -4,44 +4,20 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import no.solcellepaneller.ui.theme.SolcellepanellerTheme
+import no.solcellepaneller.data.homedata.ElectricityPriceRepository
+import no.solcellepaneller.ui.electricity.PriceScreen
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            SolcellepanellerTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
-            }
+            val repository = ElectricityPriceRepository("NO1")
+            PriceScreen(
+                onBackClick = { finish() },
+                repository = repository,
+                isDarkMode = false
+            )
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    SolcellepanellerTheme {
-        Greeting("Android")
     }
 }
