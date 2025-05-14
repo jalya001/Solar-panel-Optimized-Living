860acfa213a1d156ba2b226f8e0de418d2d6f93d
anisai <anisai@uio.no>
2025-05-14 23:58:44 +0200

    viser nåværende måned på result screen istedenfor januar

--- app/src/main/java/no/solcellepanelerApp/ui/result/ResultScreen.kt
+++ app/src/main/java/no/solcellepanelerApp/ui/result/ResultScreen.kt
@@ -44,6 +44,7 @@ import no.solcellepanelerApp.ui.handling.LoadingScreen
 import no.solcellepanelerApp.ui.reusables.DataCard
 import no.solcellepanelerApp.ui.reusables.IconTextRow
 import no.solcellepanelerApp.ui.reusables.SavingsMonth_Card
+import java.time.LocalDate
 
 @Composable
 fun ResultScreen(
@@ -144,7 +145,8 @@ private fun MonthDataDisplay(
     showAllMonths: Boolean,
 ) {
     var expanded by remember { mutableStateOf(false) }
-    var selectedMonthIndex by remember { mutableIntStateOf(0) }
+    val currentMonth = LocalDate.now().monthValue - 1 // Because monthValue is 1-12
+    var selectedMonthIndex by remember { mutableIntStateOf(currentMonth) }
     val temperatureFactors =
         resultViewModel.temperatureFactors.collectAsState().value ?: emptyList()
     val calculationResults =
